39360e00117ebb2405406d7f8948fdae6eff2f5e
ejhb <bonurajosh13@gmail.com>
2021-02-11 09:45:50 +0100

    update

---
+++ app/cogs/insult.py
@@ -0,0 +1,20 @@
+import discord
+from discord.ext import commands
+import random
+
+
+class Insult(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command(brief = "Roast someone")
+    async def roast(self, ctx, member: discord.Member = None):
+        insult = await get_momma_jokes()
+        if member is not None:
+            await ctx.send("%s  %s" %(member.name, insult))
+        else : 
+            await ctx.send(insult)
+
+
+def setup(bot):
+    bot.add_cog(Insult(bot))
