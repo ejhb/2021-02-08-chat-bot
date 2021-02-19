9df0c9f6a5f364f5aa8feaaad4093d1380a0f300
ejhb <bonurajosh13@gmail.com>
2021-02-19 11:02:28 +0100

    add

--- app/cogs/basic.py
+++ app/cogs/basic.py
@@ -1,56 +1,93 @@
 import discord
 from discord.ext import commands
+from discord.ext.commands import CommandNotFound
+from ftools import notify_user
 import random
 
 class Basic(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    #command error feedback
-    @commands.Cog.listener()
-    async def on_command_error(self, ctx, ex):
-        print(ex)
-        await ctx.send("Please check with !help, or on the manual at trucGithub.com")
+    @commands.command()
+    async def poke(self, ctx, member: discord.Member = None):
+        """Send a poke to mention user.
+        Parameters
+        ------------
+        !poke @user
+        """
+        if member is not None:
+            message = "%s Require your presence" % ctx.author.name
+            await notify_user(member, message)
+        else:
+            await ctx.send("Please use @mention to poke someone.")
+    
+    @commands.command()
+    async def dm(self, ctx, member: discord.Member = None , message: str=""):
+        """Send a dm to mention user.
+        Parameters
+        ------------
+        !dm @user "direct message"
+        """
+        if member is not None : 
+            dm = f'{ctx.author.name} say : {message}'
+            await notify_user(member, message = dm)
+        elif message == "":
+            await ctx.send("Please fill your message to dm someone")
+        else :
+            await ctx.send("Please use @mention to message someone.")
 
-    @commands.command(brief="Repeating sentence")
-    async def say(self, ctx, *args):
-        if len(args) > 0 :
-            await ctx.send(" ".join(args))
-        else : 
-            await ctx.send("It need arguments")
 
-    @commands.command(brief="Create an invite")
+    @commands.command(name='repeat', aliases=['mimic', 'copy','say'])
+    async def say(self, ctx, *, inp: str):
+        """A simple command which repeats your input!
+        Parameters
+        ------------
+        inp: str
+            The input you wish to repeat.
+        """
+        await ctx.send(inp)
+        
+    @say.error
+    async def say_handler(self, ctx, error):
+        """A local Error Handler for our command do_repeat.
+        This will only listen for errors in do_repeat.
+        The global on_command_error will still be invoked after.
+        """
+
+        if isinstance(error, commands.MissingRequiredArgument):
+            if error.param.name == 'inp':
+                await ctx.send("You forgot to give me input to repeat!")
+
+    @commands.command()
     async def invite(self, ctx):
+        '''Create an server invite.
+        Parameters
+        ------------
+        !invite
+        '''
         link = await ctx.channel.create_invite(max_age=1)
         await ctx.send(link)
 
     @commands.Cog.listener("on_message")
     async def detect(self, message):
-        x = message.content.split()
-        #Possible greetings from user
-        Cheers = ["Hi", "hi", "Hello", "hello","bonjour","salut","coucou","ciao","Hey","Yo","Sup","sup","yo","hey"]
-        unCheers= ["bye", "Bye", "cya", "cu","ciao","aurevoir","Aurevoir","a+","A+","++",]
-        #Possible greetings from bot
-        happy_emoji = [":grinning:",":smiley:",":smile:",":grin:",":laughing:",":slight_smile:",":sunglasses:"]
-        RCheers = ["Hi", "Hello","Bonjour","Salut","Coucou","It’s good to see you.","Hey","Hey there","What’s up?","Yo!","Sup?","Waddap"]
-        RunCheers= ["Bye", "cya", "cu","ciao","Aurevoir","a+","Goodbye","Bye bye!","Take it easy","Take care","Later","See you soon","I hope to see you soon"]
-        print(x)
-        if any(word in x for word in Cheers):
-            if message.author.bot:
-                return
-            else : 
-                msg = f'{random.choice(RCheers)} {message.author.mention} {random.choice(happy_emoji)}'
-                await message.channel.send(msg)
-        elif any(word in x for word in unCheers):
-            if message.author.bot:
-                return
-            else : 
-                msg = f'{random.choice(RunCheers)} {message.author.mention} {random.choice(happy_emoji)}'
-                await message.channel.send(msg)
-
+        if message.author.bot:
+            pass
+        else :
+            x = message.content.split()
+            print(x)
+            #Possible greetings from user
+            Cheers = ["Hi", "hi", "Hello", "hello","bonjour","salut","coucou","ciao","Hey","Yo","Sup","sup","yo","hey"]
+            unCheers= ["bye", "Bye", "cya", "cu","ciao","aurevoir","Aurevoir","a+","A+","++",]
+            #Possible greetings from bot
+            happy_emoji = [":grinning:",":smiley:",":smile:",":grin:",":laughing:",":slight_smile:",":sunglasses:"]
+            RCheers = ["Hi", "Hello","Bonjour","Salut","Coucou","It’s good to see you.","Hey","Hey there","What’s up?","Yo!","Sup?","Waddap"]
+            RunCheers= ["Bye", "cya", "cu","ciao","Aurevoir","a+","Goodbye","Bye bye!","Take it easy","Take care","Later","See you soon","I hope to see you soon"]
+            if any(word in x for word in Cheers):
+                    msg = f'{random.choice(RCheers)} {message.author.mention} {random.choice(happy_emoji)}'
+                    await message.channel.send(msg)
+            elif any(word in x for word in unCheers):
+                    msg = f'{random.choice(RunCheers)} {message.author.mention} {random.choice(happy_emoji)}'
+                    await message.channel.send(msg)
 
 def setup(bot):
     bot.add_cog(Basic(bot))
-
-
-# # https://stackoverflow.com/questions/65062365/discord-py-cog-problem-with-on-message-event-dont-work
--- app/main.py
+++ app/main.py
@@ -1,12 +1,13 @@
-import discord
+import discord, os
+
 from discord.ext import commands
-from settings import * 
+from settings import *
 
 
 
-description = str("Homie-bot allow theses prefix  !  /  $  . to use his/her commands. \n  Example : \n    !help    /help    $help    .help") 
+description = str("Homie-bot allow theses prefix  !   $  . to use his/her commands. \n\nExample : !help    $help    .help \n \nFor more details on commands itself you can type : !help yourcommands") 
 
-bot = commands.Bot(command_prefix = ['!','/','$','.'], description = description)
+bot = commands.Bot(command_prefix = ['!','$','.'], description = description)
 
 #add dynamicly new extension from dir.cogs
 for filename in os.listdir("./cogs"):
